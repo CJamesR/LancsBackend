d74651cc2eab11c4008d2aa7a2035c296d471bc1
CJamesR <calvinriyono@gmail.com>
2026-08-13 10:34:56 +0700

    13/08/2026 - Revert Ulang Sistem Socket.io dan Paralel

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [{
     name: 'lancs-IoT',
     script: 'server.js',
-    instances: 'max',
-    exec_mode: 'cluster',
+    instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
